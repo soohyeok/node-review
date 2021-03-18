e264f947fd772f936428d79a31383a2bad2d9383
soohyeok <soohyeok0822@gmail.com>
2021-03-18 18:14:38 -0400

    hardwriting html using res.write and res.end

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Page</title></head>");
+  res.write("<body><h1>hello!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
